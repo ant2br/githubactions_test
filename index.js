82bbcf6cbb5a09646e982e07806c3ae243de85c4
BrenerViotto <brener.viotto@a2wplataforma.com.br>
2022-06-28 09:47:09 -0300

    rodar

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Olá, teste de github Actions')
+  res.send('Olá, teste de github Actions para o projeto teste-github-actions')
 })
 
 app.listen(port, () => {
